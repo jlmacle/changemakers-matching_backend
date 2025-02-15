7eb93378a71dfdb27ffb742e9c5f34c0e3f6c28a
Jean-Louis <you@example.com>
2025-02-15 20:34:30 +0100

    Added some javadoc comments

--- src/main/java/cm/ChangeMakerMatchingBackendApplication.java
+++ src/main/java/cm/ChangeMakerMatchingBackendApplication.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * The class to run as a SpringBoot app to start the backend service
+ * The class to run as a Spring Boot application to start the backend service.
  */
 @SpringBootApplication
 public class ChangeMakerMatchingBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChangeMakerMatchingBackendApplication.class, args);
-	}
+    /**
+     * The main method to start the Spring Boot application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ChangeMakerMatchingBackendApplication.class, args);
+    }
 
 }
--- src/main/java/cm/repositories/RepresentativesRepository.java
+++ src/main/java/cm/repositories/RepresentativesRepository.java
@@ -4,7 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import cm.models.Representative;
 
-public interface  RepresentativesRepository extends JpaRepository<Representative, Integer>{
-    public Representative findByUsername(String name);
+/**
+ * Repository interface for {@link Representative} entities.
+ */
+public interface RepresentativesRepository extends JpaRepository<Representative, Integer> {
+    
+    /**
+     * Finds a representative by their username.
+     *
+     * @param username the username of the representative
+     * @return the representative with the given username
+     */
+    public Representative findByUsername(String username);
     
 }
